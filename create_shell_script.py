4b8f6ed77c9eebd47eaf08c8a8ffb9f668c42feb
Tung Thanh Le <ttungl@users.noreply.github.com>
2017-03-02 20:04:55 -0600

    Add files via upload

--- create_shell_script.py
+++ create_shell_script.py
@@ -16,7 +16,7 @@
 import os ## for chmod
 import stat
 
-def create_shell_script(num_CPU, num_GPU, gpu_type, cpu_max_inst, benchmark, net_max_inst, injection_rate):
+def create_shell_script(num_CPU, num_GPU, gpu_type, cpu_max_inst, benchmark, net_max_inst, numThreads, injection_rate):
 	## check benchmark
 	# if benchmark == '':
 	# 	benchmark = 'default_mm'
@@ -38,31 +38,37 @@ def create_shell_script(num_CPU, num_GPU, gpu_type, cpu_max_inst, benchmark, net
 	##### Note: This file is generated under m2s directory.
 	f.write('--net-report %s_net_report.out ' % benchmark)
 	
-	## splash2 benchmarks
+	## splash2-benchmarks
 	if benchmark == 'fft':
-		f.write('benchmarks/m2s-bench-splash2/fft/fft -m18 -p8 -n65536 -l4')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/fft/fft -m18 -p%0.f -n65536 -l4' % numThreads)
 	if benchmark == 'fmm':
-		f.write('benchmarks/m2s-bench-splash2/fmm/fmm.i386 8 benchmarks/m2s-bench-splash2/fmm/input')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/fmm/fmm.i386 %0.f HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/fmm/input' % numThreads)
 	if benchmark == 'lu':
-		f.write('benchmarks/m2s-bench-splash2/lu/lu.i386 -p8 -n512 -b16')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/lu/lu.i386 -p%0.f -n512 -b16' % numThreads)
 	if benchmark == 'cholesky':
-		f.write('benchmarks/m2s-bench-splash2/cholesky/cholesky.i386 -p8 benchmarks/m2s-bench-splash2/cholesky/tk14.0')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/cholesky/cholesky.i386 -p%0.f HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/cholesky/tk14.0' % numThreads)
 	if benchmark == 'barnes':
-		f.write('benchmarks/m2s-bench-splash2/barnes/barnes.i386 8 benchmarks/m2s-bench-splash2/barnes/input')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/barnes/barnes.i386 %0.f HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/barnes/input' % numThreads)
 	if benchmark == 'ocean':
-		f.write('benchmarks/m2s-bench-splash2/ocean/ocean.i386 -n130 -p8 -e1e-07 -r20000 -t28800')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/ocean/ocean.i386 -n130 -p%0.f -e1e-07 -r20000 -t28800' % numThreads)
 	if benchmark == 'raytrace':
-		f.write('benchmarks/m2s-bench-splash2/raytrace/raytrace.i386 -p8 balls4.env')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/raytrace/raytrace.i386 -p%0.f HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/raytrace/balls4.env' % numThreads)
 	if benchmark == 'water-nsquared':
-		f.write('benchmarks/m2s-bench-splash2/water-nsquared/water-nsquared.i386 8 benchmarks/m2s-bench-splash2/water-nsquared/input')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/water-nsquared/water-nsquared.i386 %0.f HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/water-nsquared/input' % numThreads)
 	if benchmark == 'water-spatial':
-		f.write('benchmarks/m2s-bench-splash2/water-spatial/water-spatial.i386 8 benchmarks/m2s-bench-splash2/water-spatial/input')
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/water-spatial/water-spatial.i386 %0.f HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/water-spatial/input' % numThreads)
 	if benchmark == 'radiosity':
-		f.write('benchmarks/m2s-bench-splash2/radiosity/radiosity.i386 -batch -p 8 -en 0.5')
-	
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/radiosity/radiosity.i386 -batch -p %0.f -en 0.5' % numThreads)
+	if benchmark == 'radix':
+		f.write('HeteroArchGen4M2S/benchmarks/m2s-bench-splash2/radix/radix.i386 -p%0.f -r4096 -n262144 -m524288' % numThreads)
+
+
+	## cuda-benchmarks
+
+
 	## test benchmark
 	if benchmark == 'default_mm':
-		f.write('HeteroArchGen4M2S/mm_multi/mm_multi_serial 8')
+		f.write('HeteroArchGen4M2S/mm_multi/mm_multi_serial %0.f' % numThreads)
 		
 	## close
 	f.close()
